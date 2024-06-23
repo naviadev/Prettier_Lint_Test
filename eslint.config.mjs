b1d653c5e9eed1e5831899ec3f79d74f17af0392
naviadev <qo2oq1020@gmail.com>
2024-06-23 16:49:01 +0900

    Feat | ESLint 커스텀 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
-
+import { testModule } from "./lib/index.js"
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
